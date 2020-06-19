7344e7ac69ab4c03abb13b684f68a398c044acc4
zsdycs <mail@lipk.org>
2020-06-19 20:56:47 +0800

    若干修改，是的修改中包含缩进的代码

--- static/js/indentation-el.js
+++ static/js/indentation-el.js
@@ -5,13 +5,14 @@
             var tag = tags[i];
             var parent = tag.parentElement;
             if (parent.nodeName === 'ARTICLE' || parent.nodeName === 'BLOCKQUOTE') {
-                var children = tag.children;
-                if (children.length === 0 || children[0].nodeName === 'IMG') continue;
+                if (tag.children.length !== 0) {
+                    if (tag.children[0].nodeName === 'IMG') continue;
+                };
                 tag.innerHTML = '&emsp;&emsp;' + tag.innerHTML;
-                for (var j = 0; j < children.length; j++) {
-                    var child = children[j];
+                for (var j = 0; j < tag.childNodes.length; j++) {
+                    var child = tag.childNodes[j];
                     if (child.nodeName === 'BR') {
-                        children[j+1].textContent = '  ' + children[j+1].textContent.substring(1, children[j+1].textContent.length);
+                        tag.childNodes[j+1].textContent = '  ' + tag.childNodes[j+1].textContent.substring(1, tag.childNodes[j+1].textContent.length);
                     }
                 }
             }
