1a67be9065ef5903e7e4ce86bdb5fa1db8de98d2
zsdycs <mail@lipk.org>
2021-04-20 14:14:30 +0800

    格式、添加 setting

--- static/js/indentation-el.js
+++ static/js/indentation-el.js
@@ -1,22 +1,22 @@
 (function () {
-    function indentation_el(tagName) {
-        var tags = document.getElementsByTagName(tagName);
-        for (var i = 0; i < tags.length; i++) {
-            var tag = tags[i];
-            var parent = tag.parentElement;
-            if (parent.nodeName === 'ARTICLE' || parent.nodeName === 'BLOCKQUOTE') {
-                if (tag.children.length !== 0) {
-                    if (tag.children[0].nodeName === 'IMG') continue;
-                };
-                tag.innerHTML = '&emsp;&emsp;' + tag.innerHTML;
-                for (var j = 0; j < tag.childNodes.length; j++) {
-                    var child = tag.childNodes[j];
-                    if (child.nodeName === 'BR') {
-                        tag.childNodes[j+1].textContent = '  ' + tag.childNodes[j+1].textContent.substring(1, tag.childNodes[j+1].textContent.length);
-                    }
-                }
-            }
+  function indentation_el(tagName) {
+    var tags = document.getElementsByTagName(tagName);
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+      var parent = tag.parentElement;
+      if (parent.nodeName === 'ARTICLE' || parent.nodeName === 'BLOCKQUOTE') {
+        if (tag.children.length !== 0) {
+          if (tag.children[0].nodeName === 'IMG') continue;
+        };
+        tag.innerHTML = '&emsp;&emsp;' + tag.innerHTML;
+        for (var j = 0; j < tag.childNodes.length; j++) {
+          var child = tag.childNodes[j];
+          if (child.nodeName === 'BR') {
+            tag.childNodes[j + 1].textContent = '  ' + tag.childNodes[j + 1].textContent.substring(1, tag.childNodes[j + 1].textContent.length);
+          }
         }
+      }
     }
-    indentation_el('p');
+  }
+  indentation_el('p');
 })();
