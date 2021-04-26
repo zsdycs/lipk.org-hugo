6131d6cdc600d76016fb3c3d5fc8ea3645d0016e
zsdycs <mail@lipk.org>
2021-04-26 10:04:46 +0800

    菜单及侧边操作按钮 在移动端样式更新

--- static/js/hide-menu.js
+++ static/js/hide-menu.js
@@ -25,6 +25,7 @@
 
   var $ = document.querySelector.bind(document);
   var menu = $('header.masthead');
+  var fixedBtn = $('#fixed-button');
   if (!menu) return;
   var oldPageYOffset = window.pageYOffset;
   var documentElementClientHeight = document.documentElement.clientHeight;
@@ -33,21 +34,36 @@
     if (window.innerWidth < 1220) {
       if (window.pageYOffset < 50) {
         menu.style.display = 'block';
+        if (fixedBtn) {
+          fixedBtn.style.display = 'block'
+        };
         return;
       };
       if (document.documentElement.scrollHeight - document.documentElement.scrollTop - 300 <= documentElementClientHeight) {
         menu.style.display = 'block';
+        if (fixedBtn) {
+          fixedBtn.style.display = 'block'
+        };
         return;
       }
       var newPageYOffset = window.pageYOffset;
       if (newPageYOffset < oldPageYOffset) {
         menu.style.display = 'block';
+        if (fixedBtn) {
+          fixedBtn.style.display = 'block'
+        };
       } else if ((newPageYOffset - oldPageYOffset) > 20) {
         menu.style.display = 'none';
+        if (fixedBtn) {
+          fixedBtn.style.display = 'none'
+        };
       }
       oldPageYOffset = newPageYOffset;
     } else {
       menu.style.display = 'block';
+      if (fixedBtn) {
+        fixedBtn.style.display = 'block'
+      };
     }
   }
   document.addEventListener('scroll', throttle(getScrollDirection, 500, 200));
