f97e6eb6011da82affb4760270fa21e70683752f
zsdycs <mail@lipk.org>
2021-06-03 09:57:30 +0000

    添加 时区

--- version.js
+++ version.js
@@ -10,22 +10,22 @@ const name = execSync('git show -s --format=%cn').toString().trim();
 const email = execSync('git show -s --format=%ce').toString().trim();
 const message = execSync('git show -s --format=%s').toString().trim();
 const date = new Date(execSync('git show -s --format=%cd').toString());
-const year = date.getFullYear();
+const year = date.getUTCFullYear();
 const monthList = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-const month = monthList[new Date().getMonth()];
-const day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate().toString();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-versionStr = `<svg class="version" width="100" height="22" viewBox="0 0 100 22" xmlns="http://www.w3.org/2000/svg">
+const month = monthList[new Date().getUTCMonth()];
+const day = date.getUTCDate() > 10 ? date.getUTCDate() : '0' + date.getUTCDate().toString();
+const hours = date.getUTCHours() > 10 ? date.getUTCHours() : '0' + date.getUTCHours().toString();
+const minutes = date.getUTCMinutes();
+versionStr = `<svg class="version" width="120" height="22" viewBox="0 0 120 22" xmlns="http://www.w3.org/2000/svg">
   <title>Last update time of this project</title>
   <desc>
-    Time: ${year + '-' + month + '-' + day + ' ' + hours + ':' + minutes}
+    Time: ${year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ' GMT'}
     Email: ${email}
     Author: ${name}
     Message: ${message}
     Version: ${commit}
   </desc>
-  <text x="0" y="15" textLength="100" font-size="16">${month + '-' + day + ' ' + hours + ':' + minutes}</text>
+  <text x="0" y="16" textLength="120" font-size="16">${month + '-' + day + ' ' + hours + ':' + minutes + ' GMT'}</text>
 </svg>`;
 if (fs.existsSync(filePath)) {
   fs.writeFileSync(`${filePath}/${fileName}`, versionStr);
