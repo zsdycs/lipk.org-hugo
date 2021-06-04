d1f89a2d8b1f71bfd8444aa0652eaf1553f66607
zsdycs <mail@lipk.org>
2021-06-04 18:16:49 +0800

    引入 photoswipe

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,7 @@ import './right-quote.js';                          // 引用文言
 import './fix-footnote.js';                         // 引用参考资料
 import './checkbox-list.js';                        // 无序复选列表
 import './hide-menu.js';                            // 菜单栏自动隐藏
-import './tableOfContents.v2.js';                      // 目录
+import './tableOfContents.v2.js';                   // 目录
 import './beaudar.js';                              // 评论
-import './load-typekit.js';                            // 字体
+import './load-typekit.js';                         // 字体
+import './picture-view.js'                          // 图片查看
---
+++ static/js/picture-view.js
@@ -0,0 +1,8 @@
+import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
+const lightbox = new PhotoSwipeLightbox({
+  // TODO
+
+  pswpModule: './photoswipe.esm.js',
+
+});
+lightbox.init();
